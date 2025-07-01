f48f22b6a6e0b585f78334daf91e74ef1be112d4
Sarthak Neupane <sarthakneup11@gmail.com>
2025-07-01 17:44:02 +0545

    discussion post image option added

--- services/forms.py
+++ services/forms.py
@@ -4,4 +4,4 @@ from .models import DiscussionPost
 class DiscussionPostForm(forms.ModelForm):
     class Meta:
         model = DiscussionPost
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'image', 'location']  # include image + location
--- services/models.py
+++ services/models.py
@@ -5,6 +5,8 @@ class DiscussionPost(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
+    image = models.ImageField(upload_to='discussion_images/', blank=True, null=True)  # NEW
+    location = models.CharField(max_length=255, blank=True, null=True)  # For storing map location
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 

7c14549a723eb5d45a4e8b2a9451d31a3ad08939
Sarthak Neupane <sarthakneup11@gmail.com>
2025-07-04 08:17:44 +0545

    free health services and tax payment cards applied

--- services/views.py
+++ services/views.py
@@ -42,8 +42,8 @@ def post_detail(request, pk):
 def service_page(request):
     services = [
         {"title": "Waste Collection", "desc": "Schedule pickups, track waste disposal, and get eco-tips."},
-        {"title": "Water Services", "desc": "Report issues, view billing, and manage usage."},
-        {"title": "Property Tax", "desc": "View bills, pay online, and download receipts."},
+        {"title": "Free health services", "desc": "Information and contacts.","url": "https://freehealth.kathmandu.gov.np/home/"},
+        {"title": "Tax Payment", "desc": "Pay online and download receipts.","url": "https://eservice.kathmandu.gov.np/"},
         {"title": "Public Parks", "desc": "Find parks, facilities, and events near you."},
         {"title": "Road Maintenance", "desc": "Report potholes, construction updates, and work schedules."},
         {"title": "Permit Applications", "desc": "Apply for permits for events, businesses, and construction."}

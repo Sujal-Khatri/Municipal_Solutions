3744017524c69d4f215a5d766da622353b1ac64e
Sarthak Neupane <sarthakneup11@gmail.com>
2025-07-05 16:06:39 +0545

    staff roles and admin panel access

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,10 @@
-# Register your models here.
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin, GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.models import Group
 from .models import CustomUser
 
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    model = CustomUser
     list_display = ['username', 'email', 'is_admin', 'is_staff']
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('is_admin', 'phone_number', 'address')}),
@@ -13,4 +13,43 @@ class CustomUserAdmin(UserAdmin):
         (None, {'fields': ('is_admin', 'phone_number', 'address')}),
     )
 
-admin.site.register(CustomUser, CustomUserAdmin)
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        if obj is None:
+            # listing view: allow staff to see the list
+            return True
+        # detail view/edit: only superusers
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_view_permission(self, request, obj=None):
+        # both superuser and staff can view
+        return request.user.is_superuser or request.user.is_staff
+
+
+class ReadOnlyGroupAdmin(BaseGroupAdmin):
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        if obj is None:
+            return True
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser or request.user.is_staff
+
+# unregister the default Group admin and re-register with our readonly version
+admin.site.unregister(Group)
+admin.site.register(Group, ReadOnlyGroupAdmin)
